bd7ccc420d4966ffd6f896891c2086171d88819a
dongrili <dongrili@outlook.com>
2015-06-14 21:24:55 +0800

    with mybatis

--- user/user-service/user-core/src/main/java/com/meitun/user/core/impl/UserFacadeServiceImpl.java
+++ user/user-service/user-core/src/main/java/com/meitun/user/core/impl/UserFacadeServiceImpl.java
@@ -2,6 +2,9 @@ package com.meitun.user.core.impl;
 
 import com.meitun.user.client.UserFacadeService;
 import com.meitun.user.common.model.User;
+import com.meitun.user.dal.domain.UserDO;
+import com.meitun.user.dal.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,13 +13,23 @@ import org.springframework.stereotype.Service;
 @Service("userFacadeService")
 public class UserFacadeServiceImpl implements UserFacadeService {
 
+    @Autowired
+    private UserMapper userMapper;
+
     public User getUserById(Long userId) {
        if(userId==null){
            return null;
        }
+       UserDO userDO = userMapper.getUserById(userId);
+
         User user = new User();
-        user.setUserId(userId);
-        user.setUserName("007:code"+userId);
+        if(userDO!=null){
+            user.setUserName(userDO.getName());
+            user.setUserId(userDO.getId());
+        }else {
+            user.setUserId(-999L);
+            user.setUserName("007:code" + "empty");
+        }
         return user;
     }
 }
